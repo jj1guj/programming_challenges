a0a8811ed4f7c5704e2a948723f645ddc16c8d9c
jj1guj <jj1guj@jj1guj.net>
2021-04-22 19:17:05 +0900

    UnionFindしそうだけど…

---
+++ 2021/week2/1098.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    int c,x,y;
+    vector<vector<int>>G(10000,vector<int>(10000));
+    while(true){
+        cin>>c>>x>>y;
+        if(c==1){
+
+        }else if(c==2){
+
+        }else if(c==3){
+
+        }else{
+            
+        }
+        if(c==0&&x==0&&y==0)break;
+    }
+}
